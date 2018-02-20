d57b20a7c7c02fc985715729fbd5609f16f460f7
Diego Velasquez <you@example.com>
2018-02-19 20:05:54 -0500

    product detail

--- inc/theme.php
+++ inc/theme.php
@@ -122,3 +122,31 @@ function bbloomer_remove_product_tabs( $tabs ) {
 }
 
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
+
+function ciprestige_woocommerce_image_dimensions() {
+	global $pagenow;
+ 
+	if ( ! isset( $_GET['activated'] ) || $pagenow != 'themes.php' ) {
+		return;
+	}
+  	$catalog = array(
+		'width' 	=> '800',	// px
+		'height'	=> '800',	// px
+		'crop'		=> 0		// true
+	);
+	$single = array(
+		'width' 	=> '1600',	// px
+		'height'	=> '600',	// px
+		'crop'		=> 0 		// true
+	);
+	$thumbnail = array(
+		'width' 	=> '120',	// px
+		'height'	=> '120',	// px
+		'crop'		=> 0 		// false
+	);
+	// Image sizes
+	update_option( 'shop_catalog_image_size', $catalog ); 		// Product category thumbs
+	update_option( 'shop_single_image_size', $single ); 		// Single product image
+	update_option( 'shop_thumbnail_image_size', $thumbnail ); 	// Image gallery thumbs
+}
+add_action( 'after_switch_theme', 'ciprestige_woocommerce_image_dimensions', 1 );
